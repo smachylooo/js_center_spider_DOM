3cabd6168da721f3c717c4998c409f7b1e2b2191
smachyloooo <smachyloooo@gmail.com>
2024-07-02 16:31:25 +0200

    make build

---
+++ index.17c9683f.js
@@ -0,0 +1,2 @@
+var t=function(t){return document.querySelector(t)},e=t(".wall"),c=t(".spider"),i=e.clientHeight,l=e.clientWidth,n=c.clientHeight;c.clientHeight,c.style.top="".concat(i/2-n/2,"px"),c.style.left="".concat(l/2-n/2,"px");
+//# sourceMappingURL=index.17c9683f.js.map
